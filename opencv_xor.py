9756051a9bd32626534a370b494d5e286f361fca
Shawn Halayka <sjhalayka@gmail.com>
2018-04-26 13:41:34 -0600

    Add files via upload

---
+++ opencv_xor.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+ann = cv2.ml.ANN_MLP_create()
+ann.setLayerSizes(np.array([2, 8, 1], dtype=np.uint8))
+ann.setTrainMethod(cv2.ml.ANN_MLP_BACKPROP, 0.0001)
+ann.setActivationFunction(cv2.ml.ANN_MLP_SIGMOID_SYM)
+ann.setTermCriteria((cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 1, 0.001 ))
+
+input_array = np.array([[1.0, 1.0]], dtype=np.float32)
+output_array = np.array([[1.0]], dtype=np.float32)
+td = cv2.ml.TrainData_create(input_array, cv2.ml.ROW_SAMPLE, output_array)
+
+ann.train(td, cv2.ml.ANN_MLP_NO_INPUT_SCALE | cv2.ml.ANN_MLP_NO_OUTPUT_SCALE)
+
+for i in range(0, 10000):
+    ann.train(td, cv2.ml.ANN_MLP_UPDATE_WEIGHTS | cv2.ml.ANN_MLP_NO_INPUT_SCALE | cv2.ml.ANN_MLP_NO_OUTPUT_SCALE)
+
+print(ann.predict(input_array))
