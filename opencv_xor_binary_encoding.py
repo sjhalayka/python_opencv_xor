a0ee165ecc39d6e2e8b7b0fd5e1511daf62906eb
Shawn Halayka <sjhalayka@gmail.com>
2018-05-24 09:33:56 -0600

    Add files via upload

--- opencv_xor_binary_encoding.py
+++ opencv_xor_binary_encoding.py
@@ -7,29 +7,23 @@ ann.setTrainMethod(cv2.ml.ANN_MLP_BACKPROP, 0.1)
 ann.setActivationFunction(cv2.ml.ANN_MLP_SIGMOID_SYM)
 ann.setTermCriteria((cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 1, 0.000001 ))
 
-input_array0 = np.array([[0.0, 0.0]], dtype=np.float32)
-output_array0 = np.array([[0.0]], dtype=np.float32)
-input_array1 = np.array([[1.0, 0.0]], dtype=np.float32)
-output_array1 = np.array([[1.0]], dtype=np.float32)
-input_array2 = np.array([[0.0, 1.0]], dtype=np.float32)
-output_array2 = np.array([[1.0]], dtype=np.float32)
-input_array3 = np.array([[1.0, 1.0]], dtype=np.float32)
-output_array3 = np.array([[0.0]], dtype=np.float32)
+input_array = np.array([ [0.0, 0.0],
+                         [1.0, 0.0],
+                         [0.0, 1.0],
+                         [1.0, 1.0],
+                         ], dtype=np.float32)
+                        
+output_array = np.array([ [0.0],
+                          [1.0],
+                          [1.0],
+                          [0.0],
+                          ], dtype=np.float32)
 
-td0 = cv2.ml.TrainData_create(input_array0, cv2.ml.ROW_SAMPLE, output_array0)
-td1 = cv2.ml.TrainData_create(input_array1, cv2.ml.ROW_SAMPLE, output_array1)
-td2 = cv2.ml.TrainData_create(input_array2, cv2.ml.ROW_SAMPLE, output_array2)
-td3 = cv2.ml.TrainData_create(input_array3, cv2.ml.ROW_SAMPLE, output_array3)
+td = cv2.ml.TrainData_create(input_array, cv2.ml.ROW_SAMPLE, output_array)
 
-ann.train(td0, cv2.ml.ANN_MLP_NO_INPUT_SCALE | cv2.ml.ANN_MLP_NO_OUTPUT_SCALE)
+ann.train(td, cv2.ml.ANN_MLP_NO_INPUT_SCALE | cv2.ml.ANN_MLP_NO_OUTPUT_SCALE)
 
 for i in range(0, 10000):
-    ann.train(td0, cv2.ml.ANN_MLP_UPDATE_WEIGHTS | cv2.ml.ANN_MLP_NO_INPUT_SCALE | cv2.ml.ANN_MLP_NO_OUTPUT_SCALE)
-    ann.train(td1, cv2.ml.ANN_MLP_UPDATE_WEIGHTS | cv2.ml.ANN_MLP_NO_INPUT_SCALE | cv2.ml.ANN_MLP_NO_OUTPUT_SCALE)
-    ann.train(td2, cv2.ml.ANN_MLP_UPDATE_WEIGHTS | cv2.ml.ANN_MLP_NO_INPUT_SCALE | cv2.ml.ANN_MLP_NO_OUTPUT_SCALE)
-    ann.train(td3, cv2.ml.ANN_MLP_UPDATE_WEIGHTS | cv2.ml.ANN_MLP_NO_INPUT_SCALE | cv2.ml.ANN_MLP_NO_OUTPUT_SCALE)
+    ann.train(td, cv2.ml.ANN_MLP_UPDATE_WEIGHTS | cv2.ml.ANN_MLP_NO_INPUT_SCALE | cv2.ml.ANN_MLP_NO_OUTPUT_SCALE)
 
-print(ann.predict(input_array0))
-print(ann.predict(input_array1))
-print(ann.predict(input_array2))
-print(ann.predict(input_array3))
+print(ann.predict(input_array))
